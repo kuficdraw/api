3e35005e97cc3a2a1b755485cae21e1a5d8612ab
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-03-04 02:31:57 +0200

    sort by "created at" in "artwork.json" file instead of system modified time

--- resources/utils/main.py
+++ resources/utils/main.py
@@ -3,6 +3,12 @@ import os
 from datetime import datetime
 
 
+def convert_timestamp_to_datetime(timestamp):
+    # Convert milliseconds to seconds
+    timestamp_seconds = timestamp / 1000.0
+    # Convert timestamp to datetime object
+    return datetime.fromtimestamp(timestamp_seconds)
+    
 def formatted_time():
     # Get the current datetime
     current_datetime = datetime.now()
@@ -32,11 +38,36 @@ def get_filenames_sorted_by_modified_time(directory):
     # Extract and return only the filenames from sorted results
     return [file_info["filename"] for file_info in results]
 
+def get_filenames_sorted_by_created_at_in_json(directory):
+    results = []
+    # Check if the directory exists
+    if os.path.exists(directory):
+        # Get all files and directories in the given directory
+        items = os.listdir(directory)
+        # Iterate through each item
+        for item in items:
+            # Check if the item is a file
+            if os.path.isfile(os.path.join(directory, item)):
+                if item != "meta.json":
+                    # Read JSON file content to extract createdAt
+                    with open(os.path.join(directory, item), 'r', encoding='utf-8') as file:
+                        file_content = file.read()
+                        try:
+                            file_json = json.loads(file_content)
+                            created_at = file_json["info"]["createdAt"]
+                            results.append({"filename": item, "createdAt": created_at})
+                        except (json.JSONDecodeError, KeyError) as e:
+                            print(f"Error processing file {item}: {e}")
+    # Sort results by createdAt datetime in descending order (most recent first)
+    results.sort(key=lambda x: convert_timestamp_to_datetime(x["createdAt"]), reverse=True)
+    # Extract and return only the filenames from sorted results
+    return [file_info["filename"] for file_info in results]
+
 if __name__ == "__main__":
     artworks_directory = "../artworks"
 
     time = formatted_time()
-    files = get_filenames_sorted_by_modified_time(artworks_directory)
+    files = get_filenames_sorted_by_created_at_in_json(artworks_directory)
     print(files);
 
     # Generate the meta dictionary
